950af7008665eb5643fe382567ddb62ea17cb4fa
Abhinav Gunwant <abhigunwant@gmail.com>
2024-06-19 23:45:42 +0530

    Fixed deprecated stuff, fixed movement added axes

--- src/game/player/systems/movement.rs
+++ src/game/player/systems/movement.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::FRAC_PI_2;
+use std::f32::consts::{ PI, FRAC_PI_2 };
 
 use bevy::{ prelude::*, input::mouse::MouseMotion };
 
@@ -57,35 +57,11 @@ pub fn move_player(
             sprint = true;
         }
 
-        // let mut camera_angle_changed = false;
-
         for mouse in mouse_er.read() {
             player.yaw -= FRAC_PI_2 * MOUSE_SENSITIVITY * mouse.delta.x;
             player.pitch -= FRAC_PI_2 * MOUSE_SENSITIVITY * mouse.delta.y;
-            player.pitch = player.pitch.clamp(-FRAC_PI_2, FRAC_PI_2);
-
-            // camera_angle_changed = true;
-
-            /*
-            if mouse_motion.delta.x != 0.0 || mouse_motion.delta.y != 0.0 {
-                println!(
-                    "Mouse moved: X: {} px, Y: {} px",
-                    mouse_motion.delta.x,
-                    mouse_motion.delta.y
-                );
-            }
-            */
+            player.pitch = player.pitch.clamp(0.0, PI);
         }
-
-        /*
-        if camera_angle_changed {
-            println!(
-                "Player rotation angle: yaw: {} radians, pitch: {} radians",
-                player.yaw,
-                player.pitch,
-            );
-        }
-        */
 
         if sprint {
             transform.translation += direction * PLAYER_RUNNING_SPEED * time.delta_seconds();
--- src/systems.rs
+++ src/systems.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::{mesh::VertexAttributeValues, render_asset::RenderAssetUsages}};
 
 pub fn setup(
     mut commands: Commands,
@@ -6,25 +6,65 @@ pub fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>
 ) {
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Circle::new(400.0)),
+        mesh: meshes.add(Circle::new(400.0)),
         material: materials.add(Color::WHITE),
         ..default()
     });
 
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
         material: materials.add(Color::RED),
         transform: Transform::from_xyz(0.0, 0.0, 0.5),
         ..default()
     });
 
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 15000.0,
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 800.0,
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        transform: Transform::from_xyz(4.0, 8.0, 4.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // Draw x-axis
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::new(
+            bevy::render::mesh::PrimitiveTopology::LineList,
+            RenderAssetUsages::RENDER_WORLD,
+        ).with_inserted_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            VertexAttributeValues::Float32x3(vec![[-10.0, 0.0, 0.0], [10.0, 0.0, 0.0]]),
+        )),
+        material: materials.add(Color::RED),
+        ..default()
+    });
+
+    // Draw y-axis
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::new(
+            bevy::render::mesh::PrimitiveTopology::LineList,
+            RenderAssetUsages::RENDER_WORLD,
+        ).with_inserted_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            VertexAttributeValues::Float32x3(vec![[0.0, -10.0, 0.0], [0.0, 10.0, 0.0]]),
+        )),
+        material: materials.add(Color::GREEN),
+        ..default()
+    });
+
+    // Draw z-axis
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::new(
+            bevy::render::mesh::PrimitiveTopology::LineList,
+            RenderAssetUsages::RENDER_WORLD,
+        ).with_inserted_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            VertexAttributeValues::Float32x3(vec![[0.0, 0.0, -10.0], [0.0, 0.0, 10.0]]),
+        )),
+        material: materials.add(Color::BLUE),
         ..default()
     });
 }
