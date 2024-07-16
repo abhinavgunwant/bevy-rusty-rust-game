93ba65950c34c3c798fc7ac8593d52ffc63b9114
Abhinav Gunwant <abhigunwant@gmail.com>
2024-07-17 03:59:02 +0530

    Console logs saved and loaded from log file

--- src/ui/dev/console/systems/layout.rs
+++ src/ui/dev/console/systems/layout.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use std::{fs::{ metadata, File, write, create_dir }, io::Read};
+
 use crate::ui::dev::console::{
     components::{ Console, ConsoleHistory, ConsoleTextLine, Scrollable },
     CONSOLE_HEIGHT, CONSOLE_TEXT_LINE_HEIGHT, CONSOLE_TITLE_HEIGHT,
@@ -18,6 +20,33 @@ pub fn spawn_console(mut commands: Commands) {
         CONSOLE_HEIGHT - CONSOLE_TEXT_LINE_HEIGHT - CONSOLE_TITLE_HEIGHT
     );
     console_history_style.overflow = Overflow::clip_y();
+
+    let mut history_lines: Vec<String> = vec![];
+
+    match metadata("logs/console.log") {
+        Ok(_) => {
+            match File::open("logs/console.log") {
+                Ok(mut file) => {
+                    let mut content: String = String::default();
+
+                    match file.read_to_string(&mut content) {
+                        Ok(_) => {
+                            history_lines = content.lines()
+                                .map(|s| s.to_string()).collect();
+                        }
+                        Err(_) => {}
+                    };
+                }
+                Err(_) => {
+                    println!("Could not read logs file");
+                }
+            }
+        }
+
+        Err(_) => {
+            println!("Could not open logs file");
+        }
+    }
 
     commands.spawn((
         NodeBundle {
@@ -70,19 +99,41 @@ pub fn spawn_console(mut commands: Commands) {
             },
             Scrollable { pos: 0.0 },
         )).with_children(|history| {
+            let position_top;
+
+            if history_lines.len() > 12 {
+                position_top = -20.0 * (history_lines.len() as f32 - 12.0);
+            } else {
+                position_top = 0.0;
+            }
+
+            let text_sections = history_lines.clone().into_iter().map(
+                |line| TextSection {
+                    value: format!("\n{}", line),
+                    style: TextStyle {
+                        font_size: 20.0,
+                        ..default()
+                    }
+                }
+            ).collect();
+
             history.spawn((
                 TextBundle {
                     text: Text {
-                        sections: vec![],
+                        sections: text_sections,
                         ..default()
                     },
                     style: Style {
                         flex_direction: FlexDirection::Column,
+                        top: Val::Px(position_top),
                         ..default()
                     },
                     ..default()
                 },
-                ConsoleHistory { text_vec: vec![], position: 0.0 },
+                ConsoleHistory {
+                    text_vec: history_lines.clone(),
+                    position: position_top,
+                },
             ));
         });
 
@@ -120,12 +171,34 @@ pub fn spawn_console(mut commands: Commands) {
 
 pub fn despawn_console(
     mut commands: Commands,
-    console_query: Query<Entity, With<Console>>
+    console_query: Query<Entity, With<Console>>,
+    console_hist_query: Query<&mut ConsoleHistory, With<ConsoleHistory>>,
 ) {
+    if let Ok(console_history) = console_hist_query.get_single() {
+        match metadata("logs/console.log") {
+            Ok(_) => {}
+            Err(_) => {
+                match create_dir("logs") {
+                    Ok(_) => {
+                        println!("Created logs directory");
+                    }
+                    Err(e) => {
+                        println!("Could not create logs directory: {}", e);
+                    }
+                }
+            }
+        }
+
+        match write("logs/console.log", console_history.text_vec.join("\n")) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("could not write console log to file: {}", e);
+            }
+        };
+    }
+
     if let Ok(console_entity) = console_query.get_single() {
         commands.entity(console_entity).despawn_recursive();
     }
-
-    // TODO: store the command history in a file or a Local<...> here...
 }
 
